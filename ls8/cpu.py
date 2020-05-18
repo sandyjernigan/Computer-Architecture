f39745b92b7cb3b6de073782d79c19df32308e7e
Sandy <sandy.jernigan@gmail.com>
2020-05-18 02:13:56 -0400

    If less-than flag is set (true), jump to the address stored in the given register.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -390,11 +390,14 @@ class CPU:
                 self.OPS("JMP")
             else: 
                 self.pc += 2
-
-        # TODO
         elif op == "JLT":
             """ If `less-than` flag is set (true), jump to the address stored in the given register. """
-            pass
+            # Check flag
+            flag = self.reg[self.FL]
+            if (int(flag >> 2, 2) & 1):
+                self.OPS("JMP")
+            else: 
+                self.pc += 2
         elif op == "JMP":
             """ Jump to the address stored in the given register. """
             # Set the `PC` to the address stored in the given register.
